f236babd99c17424357466291d4263448e064386
Muhammad Ilham <113502296+J1gXaW@users.noreply.github.com>
2023-01-26 22:52:46 +0700

    Add files via upload

---
+++ pelanggan-service/src/main/java/penjualan.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author SMKN2LUBAS6-12
+ */
+public class penjualan {
+    
+}
---
+++ pelanggan-service/src/test/java/com/ilham/pelanggan/PelangganServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilham.pelanggan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PelangganServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
